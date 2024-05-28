221a2e4b496eaafefbd49336d8d7c14f7f426266
Eric07110904 <azsx26735546@gmail.com>
2024-05-28 08:02:44 +0800

    更新規則

--- two.c
+++ two.c
@@ -3,9 +3,9 @@
 
 int main(){
     printf("two two two two 1111111\n");
-    for(int i=0; i<10000; i++){
+    for(int i=0; i<10000; i+=2){
         printf("%d", i);
-        printf("\n\n");
+        printf("\n\n\n");
     }
     printf("tttttttttttttttttttttt");
     return 0;
