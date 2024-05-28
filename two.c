776b026dfcdf42bc2d41f9ff1a34a9c2bbd0357a
CallumQiu <m11015008@gapps.ntust.edu.tw>
2024-05-28 08:03:37 +0800

    新規則

--- two.c
+++ two.c
@@ -3,8 +3,9 @@
 
 int main(){
     printf("two two two two 1111111\n");
-    for(int i=0; i<10000; i++){
+    for(int i=0; i<10000; i+=3){
         printf("%d", i);
+        printf("yo");
         printf("\n\n");
     }
     printf("tttttttttttttttttttttt");
